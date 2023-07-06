784842d768f4cc0795e8f505c7c30b7a0132d82b
Joy Paul <joypaul3592@gmail.com>
2023-07-07 00:21:29 +0600

    aadding

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-// import Main from './Component/BartaMain/Main';
 import Tmain from './Component/Translator/Tmain';
 
 
 function App() {
-
-
-
   return (
-
     <div>
       <Tmain />
     </div>
